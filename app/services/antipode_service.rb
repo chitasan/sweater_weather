1e813b0860ffd3b6cb9982e01ef379228b491bfd
Chi Tran <chidtran@gmail.com>
2019-06-03 10:16:44 -0600

    Adds antipode service spec

--- app/services/antipode_service.rb
+++ app/services/antipode_service.rb
@@ -1,15 +1,46 @@
 class AntipodeService
   def initialize(lat, long)
-    @lat  = lat
-    @long = long
+    @lat  = lat.to_i
+    @long = long.to_i
+  end
+
+  def get_lat
+    get_lat_long[:lat].to_s
+  end
+
+  def get_long
+     get_lat_long[:long].to_s
   end 
 
-  private 
+  private
+
+  def get_lat_long
+    get_antipode_json[:data][:attributes]
+  end 
+
+  def get_antipode_json
+    response = antipode_conn.get("/api/v1/antipodes?lat=#{@lat}&long=#{@long}")
+    JSON.parse(response.body, symbolize_names: true)
+  end 
 
   def antipode_conn
-    Faraday.new(url: 'http://amypode.herokuapp.com') do |faraday|
+    Faraday.new(url: 'http://amypode.herokuapp.com/') do |faraday|
       faraday.headers['api_key'] = ENV['AMYPODE_API_KEY']
       faraday.adapter Faraday.default_adapter
     end
   end 
 end 
+
+# class AmypodeService < ApplicationService
+#   def get_antipode(location)
+#     lat = location.latitude.to_i
+#     long = location.longitude.to_i
+#     get_json("/api/v1/antipodes?lat=#{lat}&long=#{long}", authenticate!)
+#   end
+
+#   private
+
+#   def authenticate!
+#     ENV['AMYPODE_KEY']
+#   end
+# end
--- spec/services/antipode_service_spec.rb
+++ spec/services/antipode_service_spec.rb
@@ -2,14 +2,24 @@ require 'rails_helper'
 
 RSpec.describe AntipodeService do
   it 'exists' do 
-    service = AntipodeService.new('city', 'st')
+    service = AntipodeService.new('lat', 'long')
 
     expect(service).to be_a(AntipodeService)
   end 
 
-  # describe 'instance methods' do
-  #   it '.get_antipode' do
-  #     service = AntipodeService.new
-  #   end
-  # end
+  describe 'instance methods' do
+    it '.get_lat' do
+      service = AntipodeService.new("22.3193", "114.1694")
+      antipode_lat = service.get_lat
+
+      expect(antipode_lat).to be_a(String)
+    end
+
+    it '.get_long' do
+      service = AntipodeService.new("22.3193", "114.1694")
+      antipode_long = service.get_long
+
+      expect(antipode_long).to be_a(String)
+    end
+  end
 end
