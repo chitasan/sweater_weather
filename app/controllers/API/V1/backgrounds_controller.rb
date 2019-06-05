2f6c303fbca1e257b3b621458696085977bb2ca9
Chi Tran <44073660+chitasan@users.noreply.github.com>
2019-06-05 09:25:07 -0600

    Delete backgrounds_controller.rb

--- app/controllers/API/V1/backgrounds_controller.rb
+++
@@ -1,6 +0,0 @@
-class Api::V1::BackgroundsController < ApplicationController
-  def show
-    background = Background.new(params[:location])
-    render json: BackgroundsSerializer.new(background)
-  end
-end
